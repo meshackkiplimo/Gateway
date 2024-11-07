083047dac2186807bdaf839e207a5c0db2e441c6
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-11-07 16:30:55 +0300

    Top doctors page

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,15 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { doctors } from "../assets/assets";
+import React, { useContext } from "react";
+
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const TopDoctors = () => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-
-
-
+  const { doctors } = useContext(AppContext);
 
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
@@ -21,23 +19,25 @@ const TopDoctors = () => {
         {/* Add doctor cards here */}
         {doctors.slice(0, 10).map((item, index) => (
           <div
-          onClick={() => navigate(`/appointment/${item._id}`)}
+            onClick={() => navigate(`/appointment/${item._id}`)}
             key={index}
             className="border border-green-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 "
           >
             <img className="bg-green-50" src={item.image} alt={item.name} />
             <div className="p-4">
               <div className="flex items-center text-sm gap-4 text-center text-green-500">
-                <p className="w-2 h-2 bg-green-500 rounded-full" ></p>
+                <p className="w-2 h-2 bg-green-500 rounded-full"></p>
                 <p>available</p>
               </div>
             </div>
-            <p className='text-gray-900 text-lg font-medium' >{item.name}</p>
-            <p className='text-gray-600 text-sm '>{item.speciality}</p>
+            <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+            <p className="text-gray-600 text-sm ">{item.speciality}</p>
           </div>
         ))}
       </div>
-      <button className='bg-green-300 text-gray-600 px-12 py-13 rounded-full' >More</button>
+      <button onClick={()=>{navigate('/doctors');scrollTo(0,0)}} className="bg-green-300 text-gray-600 px-12 py-13 rounded-full">
+        More
+      </button>
     </div>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,17 @@
+// eslint-disable-next-line no-unused-vars
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  
   </BrowserRouter>,
 )
